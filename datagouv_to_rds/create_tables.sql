ef5bc380852bf6bdfe88a9cc6a069b9d51b05931
rastakoer <kev.le.grand@orange.fr>
2023-11-28 07:39:56 +0100

    changement schema Bdd + remplissage tables depuis le notebook

--- datagouv_to_rds/create_tables.sql
+++ datagouv_to_rds/create_tables.sql
@@ -8,7 +8,7 @@
 #------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS REGIONS(
-        ID_REGION   Int  Auto_increment  NOT NULL ,
+        ID_REGION   Varchar (50) NOT NULL ,
         Name_region Varchar (50) NOT NULL
 	,CONSTRAINT REGIONS_PK PRIMARY KEY (ID_REGION)
 )ENGINE=InnoDB;
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS REGIONS(
 CREATE TABLE IF NOT EXISTS DEPARTEMENTS(
         ID_DEPT          Varchar (3) NOT NULL ,
         Name_departement Varchar (50) NOT NULL ,
-        ID_REGION        Int NOT NULL
+        ID_REGION        Varchar (50) NOT NULL
 	,CONSTRAINT DEPARTEMENTS_PK PRIMARY KEY (ID_DEPT)
 
 	,CONSTRAINT DEPARTEMENTS_REGIONS_FK FOREIGN KEY (ID_REGION) REFERENCES REGIONS(ID_REGION)
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS VENTES(
         CODE_POSTAL     Int NOT NULL ,
         LONGITUDE       Decimal (9,6) NOT NULL ,
         LATITUDE        Decimal (9,6) NOT NULL ,
-        DATE            Datetime NOT NULL ,
+        DATE_MUTATION   Datetime NOT NULL ,
         SURFACE_BATI    Int NOT NULL ,
         NB_PIECES       Int NOT NULL ,
         SURFACE_TERRAIN Int NOT NULL ,
