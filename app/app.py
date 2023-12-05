aa4ba0f45d8097e188b3a0ccca9dc90f26bda6c1
rastakoer <kev.le.grand@orange.fr>
2023-12-05 17:04:41 +0100

    ajout de folium + function.py avec fonction affichage_ventes_proximite + maj requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,21 +1,9 @@
 import streamlit as st
-import pymysql
-import os
-from dotenv import load_dotenv
+from functions import *
 
 #//////////////////////////////////////////////////////////////////////////////
-#                      Chargement des variable en local
+#                          Configuration de la page
 #//////////////////////////////////////////////////////////////////////////////
-load_dotenv(dotenv_path="/home/rastakoer/Bureau/certif_app_immo/app/.venv/.local")
-#//////////////////////////////////////////////////////////////////////////////
-#                          Database RDS AWS
-#//////////////////////////////////////////////////////////////////////////////
-db = pymysql.connect(host=os.environ['DB_HOST'],
-user=os.environ['DB_USER'],
-password=os.environ['DB_PASSWORD'],
-port=int(os.environ['DB_PORT']))
-cursor= db.cursor()
-cursor.execute("USE datagouv;")
 
 # Définir le titre et le logo de l'application
 st.set_page_config(
@@ -24,92 +12,120 @@ st.set_page_config(
     layout="wide"
 )
 
+# Ajout du style CSS pour supprimer la marge haute et centrer le logo
+st.markdown(
+    """
+    <style>
+        body {
+            margin-top: 0;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-# Affichage du logo
-st.image("./Logo.png", width=200, caption="Votre Logo")
 
-# Ajout d'un menu déroulant avec les régions:
-query="""
-SELECT Name_region FROM REGIONS;
-"""
-cursor.execute(query)
-resultats = cursor.fetchall()
-liste_regions=[row[0] for row in resultats]
-
-option_par_defaut = "Sélectionnez une région"
-region = st.selectbox("Sélectionnez une région", 
-                              [option_par_defaut]+liste_regions)
-
-if region!="Sélectionnez une région":
-    # Ajout d'un menu déroulant avec les départements:
-    query=f"""
-    SELECT * FROM DEPARTEMENTS AS A
-    JOIN (
-    SELECT ID_REGION, Name_region
-    FROM REGIONS
-    )AS B
-    ON A.ID_REGION=B.ID_REGION
-    WHERE B.Name_region='{region}';
+def main():
+    # Affichage du logo
+    st.sidebar.markdown('<div><img src="Logo.png" alt="Logo" width="200"></div>', unsafe_allow_html=True)
+    
+    valid_formulaire=False
+    #//////////////////////////////////////////////////////////////////////////////
+    #                          Remplissage du formulaire
+    #//////////////////////////////////////////////////////////////////////////////
+    # Ajout d'un menu déroulant avec les régions:
+    query="""
+    SELECT Name_region FROM REGIONS;
     """
     cursor.execute(query)
     resultats = cursor.fetchall()
-    liste_departements=[row[1] for row in resultats]
-    option_par_defaut="Sélecionnez un département"
-    departement = st.selectbox("Sélecionnez un département", 
-                              [option_par_defaut]+liste_departements)
-    
+    liste_regions=[row[0] for row in resultats]
 
-    if departement!="Sélecionnez un département":
-        # Ajout d'un menu déroulant avec les communes
+    option_par_defaut = "Sélectionnez une région"
+    region = st.sidebar.selectbox("Sélectionnez une région", 
+                                [option_par_defaut]+liste_regions)
+
+    if region!="Sélectionnez une région":
+        # Ajout d'un menu déroulant avec les départements:
         query=f"""
-        SELECT * FROM COMMUNES AS A
+        SELECT * FROM DEPARTEMENTS AS A
         JOIN (
-        SELECT ID_DEPT, Name_departement
-        FROM DEPARTEMENTS
+        SELECT ID_REGION, Name_region
+        FROM REGIONS
         )AS B
-        ON A.ID_DEPT=B.ID_DEPT
-        WHERE B.Name_departement='{departement}';
+        ON A.ID_REGION=B.ID_REGION
+        WHERE B.Name_region='{region}';
         """
         cursor.execute(query)
         resultats = cursor.fetchall()
-        liste_communes=[row[1] for row in resultats]
-        option_par_defaut="Sélecionnez une commune"
-        commune = st.selectbox("Sélecionnez une commune", 
-                              [option_par_defaut]+liste_communes)
-        if commune!="Sélecionnez une commune":
-            type_de_bien = st.selectbox("Sélectionnez le type de logement :", 
-                                    ["Faites votre choix"]+["Maison", "Appartement"])
-            if type_de_bien!="Faites votre choix":
-                nb_pieces=st.selectbox("De combien de pièces est composé le bien", 
-                                [0]+[i for i in range(0,15)])
-                if nb_pieces!=0:
-                    surface_bati = st.text_input("Surface habitable", "")
-                    if surface_bati:
-                        if type_de_bien=="Maison":
-                            surface_terrain=st.text_input("Surface du terrain", "")
-                            if surface_terrain:
-                                valid_formulaire = st.button("Cliquez ici")
-                            else:
-                                st.write("Veuillez entrer la surface de votre terrain")
-                        else:
-                            surface_terrain=0
-                            valid_formulaire = st.button("Cliquez ici")
-                    else:
-                        st.write ("Veuillez indiquer une surface habitable")
-                else:
-                    st.write("Sélectionnez le nombre de pièces")
-            else:
-                st.write("Sélectionner un type de bien")
-        else:
-            st.write("Sélecionnez une commune")
-    else:
-        st.write("Veuillez choisir un département")
-else:
-    st.write("Veuillez choisir une région")
+        liste_departements=[row[1] for row in resultats]
+        option_par_defaut="Sélecionnez un département"
+        departement = st.sidebar.selectbox("Sélecionnez un département", 
+                                [option_par_defaut]+liste_departements)
+        
 
-if valid_formulaire:
-    st.title("Le formulaire fonctionne plus qu'a réaliser le modèle et créer une api !!!")
-    cursor.close()
-    db.close()
-else:
-    pass
+        if departement!="Sélecionnez un département":
+            # Ajout d'un menu déroulant avec les communes
+            query=f"""
+            SELECT * FROM COMMUNES AS A
+            JOIN (
+            SELECT ID_DEPT, Name_departement
+            FROM DEPARTEMENTS
+            )AS B
+            ON A.ID_DEPT=B.ID_DEPT
+            WHERE B.Name_departement='{departement}';
+            """
+            cursor.execute(query)
+            resultats = cursor.fetchall()
+            liste_communes=[row[1] for row in resultats]
+            option_par_defaut="Sélecionnez une commune"
+            commune = st.sidebar.selectbox("Sélecionnez une commune", 
+                                [option_par_defaut]+liste_communes)
+            if commune!="Sélecionnez une commune":
+                type_de_bien = st.sidebar.selectbox("Sélectionnez le type de logement :", 
+                                        ["Faites votre choix"]+["Maison", "Appartement"])
+                if type_de_bien!="Faites votre choix":
+                    nb_pieces=st.sidebar.selectbox("De combien de pièces est composé le bien", 
+                                    [0]+[i for i in range(0,15)])
+                    if nb_pieces!=0:
+                        surface_bati = st.sidebar.text_input("Surface habitable", "")
+                        if surface_bati:
+                            if type_de_bien=="Maison":
+                                surface_terrain=st.sidebar.text_input("Surface du terrain", "")
+                                if surface_terrain:
+                                    valid_formulaire = st.sidebar.button("Valider")
+                                else:
+                                    st.write("Veuillez entrer la surface de votre terrain")
+                            else:
+                                surface_terrain=0
+                                valid_formulaire = st.button("Valider")
+                        else:
+                            st.write ("Veuillez indiquer une surface habitable")
+                    else:
+                        st.write("Sélectionnez le nombre de pièces")
+                else:
+                    st.write("Sélectionner un type de bien")
+            else:
+                st.write("Sélecionnez une commune")
+        else:
+            st.write("Veuillez choisir un département")
+    else:
+        st.write("Veuillez choisir une région")
+
+
+    #//////////////////////////////////////////////////////////////////////////////
+    #                          Actions à produire si formulaire valide
+    #//////////////////////////////////////////////////////////////////////////////
+    if valid_formulaire:
+        st.title("Le formulaire fonctionne plus qu'a réaliser le modèle et créer une api !!!")
+        # Affichage des ventes réalisées dans la commune
+        st.components.v1.html(affichage_ventes_proximite([region,departement,commune]), height=500)
+        cursor.close()
+        db.close()
+    else:
+        pass
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/functions.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import pymysql
+import os
+from dotenv import load_dotenv
+
+# Librairies pour afficher les ventes sur une carte
+import folium
+import geopandas as gpd
+from folium.plugins import MarkerCluster
+
+#//////////////////////////////////////////////////////////////////////////////
+#                      Chargement des variable en local
+#//////////////////////////////////////////////////////////////////////////////
+load_dotenv(dotenv_path="/home/kevin/workspace/certif_app_immo/app/.venv/.local")
+#//////////////////////////////////////////////////////////////////////////////
+#                          Database RDS AWS
+#//////////////////////////////////////////////////////////////////////////////
+db = pymysql.connect(host=os.environ['DB_HOST'],
+user=os.environ['DB_USER'],
+password=os.environ['DB_PASSWORD'],
+port=int(os.environ['DB_PORT']))
+cursor= db.cursor()
+cursor.execute("USE datagouv;")
+
+def affichage_ventes_proximite(commune : list) -> str:
+    """
+    Fonction permettant d'afficher sur une carte les ventes effecuées sur une commune  
+    à l'aide de Folium
+
+    Args :
+        liste : ['Normandie','Calvados','Caen']
+    Returns:
+        str : <html>.....</html>
+    """
+    # Requête pour selectioner les ventes sur la communes
+    query=f"""
+    SELECT 
+        v.*,
+        t.NAME_TYPE_BIEN,
+        c.NAME_COMMUNE
+    FROM VENTES v
+    JOIN COMMUNES c ON v.ID_COMMUNE = c.ID_COMMUNE
+    JOIN DEPARTEMENTS d ON c.ID_DEPT = d.ID_DEPT
+    JOIN REGIONS r ON d.ID_REGION = r.ID_REGION
+    JOIN TYPES_BIENS t ON t.ID_TYPE_BIEN=v.ID_TYPE_BIEN
+    WHERE c.NAME_COMMUNE = '{commune[2]}' 
+        AND d.Name_departement = '{commune[1]}' 
+        AND r.Name_region = '{commune[0]}';
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    # Transformation de la requête en dataframe
+    df = pd.DataFrame(result)
+    df.columns= ["id","MONTANT","NUMERO_RUE","RUE","CODE_POSTAL","longitude","latitude",
+                 "DATE_MUTATION","SURFACE_BATI","SURFACE_TERRAIN","NOMBRE_PIECES",
+                 "ID_TYPE_BIEN","CODE_COMMUNE","NAME_TYPE_BIEN","NAME_COMMUNE"]
+    print(df)
+    df['info'] = df.apply(lambda row: f"""
+    Type de bien : <br>{row['NAME_TYPE_BIEN']} <br><br>
+    Date de vente : <br>{row['DATE_MUTATION']} <br><br>
+    Adresse : <br>{row['NUMERO_RUE']} {row['RUE']} <br>
+    {row['CODE_POSTAL']} {row['NAME_COMMUNE']} <br><br>
+    Surface habitable : {row['SURFACE_BATI']} <br>
+    Surface terrain : {row['SURFACE_TERRAIN']} <br>
+    Nombre de pièces : {row['NOMBRE_PIECES']} <br><br>
+    Montant : <br>{row['MONTANT']}
+    """, axis=1)
+
+    print(df.columns.to_list())
+                
+
+    # Centrage de la carte au milieu des coordonnées
+    folium_map = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=6)
+
+    # Création d'un cluster de marqueurs
+    marker_cluster = MarkerCluster()
+
+    # Ajout des marqueurs avec les information pour chaque point au cluster
+    for _,row in df.iterrows():
+        location = row['latitude'], row['longitude']
+        marker = folium.Marker(location=location)
+        folium.Popup(f'<div style="font-size: 10px; width: 150px;">{row["info"]}</div>').add_to(marker)
+        marker_cluster.add_child(marker)
+    
+    # Ajout des cluster à la map
+    marker_cluster.add_to(folium_map)
+
+    # Convertir la carte Folium en HTML
+    folium_html = folium_map.get_root().render()
+    return  folium_html
