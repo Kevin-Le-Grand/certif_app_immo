2dbf01ba13fed17233a69df80c797cd3267d9ea9
rastakoer <kev.le.grand@orange.fr>
2023-12-06 15:29:07 +0100

    ajout EDA.ipynb + maj gitignore create_tables.sql Test_scraping_rds_s3

--- datagouv_to_rds/create_tables.sql
+++ datagouv_to_rds/create_tables.sql
@@ -62,13 +62,14 @@ CREATE TABLE IF NOT EXISTS VENTES(
         MONTANT         Int NOT NULL ,
         NUMERO_RUE      Varchar (50) NOT NULL ,
         RUE             Varchar (50) NOT NULL ,
-        CODE_POSTAL     Int NOT NULL ,
+        CODE_POSTAL     Varchar (50) NOT NULL ,
         LONGITUDE       Decimal (9,6) NOT NULL ,
         LATITUDE        Decimal (9,6) NOT NULL ,
         DATE_MUTATION   Datetime NOT NULL ,
         SURFACE_BATI    Int NOT NULL ,
         NB_PIECES       Int NOT NULL ,
         SURFACE_TERRAIN Int NOT NULL ,
+        DEPENDANCES     Varchar (50) NOT NULL ,
         ID_TYPE_BIEN    Int NOT NULL ,
         ID_COMMUNE      Varchar (50) NOT NULL
 	,CONSTRAINT VENTES_PK PRIMARY KEY (ID_VENTE)
