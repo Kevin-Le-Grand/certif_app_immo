acfd00c86c6599a702d4589416cdf59a4107df66
rastakoer <kev.le.grand@orange.fr>
2023-12-08 16:38:56 +0100

    maj requirements + add app.py et function.py

---
+++ api/app.py
@@ -0,0 +1,3 @@
+from fastapi import FastAPI
+import uvicorn
+import functions
---
+++ api/functions.py
@@ -0,0 +1,6 @@
+import joblib
+import numpy as np
+from pydantic import BaseModel
+import os
+import mlflow
+import boto3
