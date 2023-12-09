2c85ee025a159d403a2d10dc2613f3a9cc3a1c88
rastakoer <kev.le.grand@orange.fr>
2023-12-09 15:32:49 +0100

    Récupération des données sur RDS + ajouts du csv dans gitignore + ajout sqlalchemy dans connection

--- model/connection.py
+++ model/connection.py
@@ -2,13 +2,18 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
 
 
 #//////////////////////////////////////////////////////////////////////////////
 #                      Chargement des variable en local
 #//////////////////////////////////////////////////////////////////////////////
-load_dotenv(dotenv_path="/home/kevin/workspace/certif_app_immo/datagouv_to_rds/.venv/.local")
+load_dotenv(dotenv_path="/home/kevin/workspace/certif_app_immo/model/.venv/.local")
 
+
+os.environ['AWS_ACCESS_KEY_ID'] = os.environ['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY'] = os.environ['AWS_SECRET_ACCESS_KEY']
 
 #//////////////////////////////////////////////////////////////////////////////
 #                          Database RDS AWS
@@ -18,3 +23,16 @@ user=os.environ['DB_USER'],
 password=os.environ['DB_PASSWORD'],
 port=int(os.environ['DB_PORT']))
 cursor= db.cursor()
+
+def connection_with_sqlalchemy(name_db : str) -> Engine:
+    """
+    Établit une connexion avec une base de données MySQL en utilisant SQLAlchemy.
+
+    Args :
+    - name_db (str) : Le nom de la base de données à laquelle se connecter.
+
+    Return :
+    sqlalchemy.engine.base.Engine : Un objet Engine SQLAlchemy représentant la connexion à la base de données.
+    """
+    engine = create_engine(f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{name_db}")
+    return engine
