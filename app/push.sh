6348b91b9287af9c965fc7806ad46467ee17f38e
rastakoer <kev.le.grand@orange.fr>
2024-01-03 16:18:01 +0100

    push before remove push.sh

--- app/push.sh
+++ app/push.sh
@@ -5,25 +5,28 @@ heroku login
 heroku container:login
 
 # Create Heroku app
-heroku create immo_app
+heroku create immoappkevleg2 --region eu
 
 # Build Image MAC ARM
 # docker buildx build --platform linux/amd64 -t streamlit-isen-g1  .
 
 # Build Image
-docker build . -t immo_app
+docker build -t immoappkevleg2 .
 
 # Tag Image to Heroku app
-docker tag immo_app registry.heroku.com/application_immo/web
+docker tag immoappkevleg2 registry.heroku.com/immoappkevleg2/web
 
 # Push Image to Heroku
-docker push registry.heroku.com/application_immo/web
+docker push registry.heroku.com/immoappkevleg2/web
 
 # Release Image to Heroku
-heroku container:release web -a application_immo
+heroku container:release web -a immoappkevleg2
+
+# Environ keys
+heroku config:set AWS_ACCESS_KEY_ID=AKIA5JTHFGQOCZZAOYY2 AWS_SECRET_ACCESS_KEY=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 DB_HOST=datagouv.c4yi8wliu37x.eu-west-3.rds.amazonaws.com DB_USER=kevappimmo DB_PASSWORD=KevAppImmo50! DB_PORT=3306 -a immoappkevleg2
 
 # Open Heroku app
-heroku open -a application_immo
+heroku open -a immoappkevleg2
 
 # Logs
-heroku logs --tail -a application_immo
+heroku logs --tail -a immoappkevleg2
