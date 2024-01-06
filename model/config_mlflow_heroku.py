07903d09b7470ab8c670ee48b08cbee27de5c412
rastakoer <kev.le.grand@orange.fr>
2024-01-06 08:40:39 +0100

    modif mlflow.ui par mlflow.start()

--- model/config_mlflow_heroku.py
+++ model/config_mlflow_heroku.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     mlflow.set_tracking_uri(os.environ.get("BACKEND_STORE_URI"))
     mlflow.sklearn.autolog()
-    mlflow.ui(port=port)
+    mlflow.start()
 
