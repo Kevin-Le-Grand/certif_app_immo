9360cbc35802a2cf2cecc9e6762df714c68d5167
rastakoer <kev.le.grand@orange.fr>
2024-01-06 08:23:18 +0100

    test mlflow heroku

--- model/config_mlflow_heroku.py
+++ model/config_mlflow_heroku.py
@@ -1,6 +1,11 @@
 import mlflow
 import os
 
-variable = os.environ.get("BACKEND_STORE_URI")
-print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",variable)
-mlflow.sklearn.autolog()
+if __name__ == "__main__":
+    variable = os.environ.get("BACKEND_STORE_URI")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",variable)
+    port = int(os.environ.get("PORT", 5000))
+    mlflow.set_tracking_uri(os.environ.get("BACKEND_STORE_URI"))
+    mlflow.sklearn.autolog()
+    mlflow.ui(port=port)
+
