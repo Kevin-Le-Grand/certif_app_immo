eb0b42fb1cac3d038de08b51228631700415a108
rastakoer <kev.le.grand@orange.fr>
2024-01-09 17:41:14 +0100

    Préparation pour le remplissage des pages

--- app/app.py
+++ app/app.py
@@ -5,39 +5,46 @@ from functions import *
 #                          Main
 #//////////////////////////////////////////////////////////////////////////////
 def main():
+    #*************************
     # Page d'authentification
+    #*************************
     if 'username' not in st.session_state:
         authentification_page(conn)
 
-    # Si l'authentification est réussie => accès 
+    #*****************************************************************
+    # Si l'authentification est réussie => accès au site de prédiction
+    #*****************************************************************
     elif st.session_state.username:
+        # Nettoyage de la page
         st.empty()
-        # Affichage du logo
+        # Affichage du logo dans la sidebar
         st.sidebar.markdown("""<div style="display: flex; justify-content: center; align-items: center;">
                             <img src="https://raw.githubusercontent.com/rastakoer/certif_app_immo/application/app/Logo.png" alt="Logo" width="200">
                             </div>""", unsafe_allow_html=True)
 
         # État du formulaire False => formulaire non validé
         st.session_state.valid_formulaire=False
-        
         # Fonction pour remplir le formulaire
         formulaire(cursor)
         
-        # Actions à produire si formulaire valide
+        #----------------------------------------------------------------------------
+        # Actions à produire en fonction de l'avancement du remplissage du formulaire 
+        #----------------------------------------------------------------------------
+        # Affichage de l'estimation
         if st.session_state.valid_formulaire:
-            # Appel de l'API
-            prediction = api_predict({'SURFACE_BATI' :st.session_state.surface_bati,
-                                    'NB_PIECES' : st.session_state.nb_pieces,
-                                    'NAME_TYPE_BIEN':st.session_state.type_de_bien,
-                                    'Name_region' : st.session_state.region})
-            # Affichage de la prédiction
-            st.title(f"Le bien est estimé à {int(prediction['reponse'])} €")
-
-            # Affichage des ventes réalisées dans la commune
-            st.components.v1.html(affichage_ventes_proximite([st.session_state.region,
-                                                            st.session_state.departement,
-                                                            st.session_state.commune],cursor), height=500)
-
+            formulaire_valide()
+        # Affichage des statistiques sur la commune :
+        elif st.session_state.commune!="Sélectionnez une commune":
+            st.title("Page sur les statistiques de la commune à venir...")
+        # Affichage des statistiques sur le département :
+        elif st.session_state.departement!="Sélectionnez un département":
+            st.title("Page sur les statistiques de la commune à venir...")
+        # Affichage des statistiques sur la région
+        elif st.session_state.region!="Sélectionnez une région":
+            st.title("Page sur les statistiques de la commune à venir...")
+        # Affichage de la page d'accueil
+        else:
+            st.title("Page d'accueil venir...")
 
 
 if __name__ == "__main__":
@@ -54,7 +61,7 @@ if __name__ == "__main__":
     """, unsafe_allow_html=True)
 
     #///////////////////////////////////////////////////////////////////////////
-    #         Connection à la base de données et création d'une table users
+    #  Connection à la base de données et création d'une table users si besoin
     #///////////////////////////////////////////////////////////////////////////
     # Connection avec la base de données
     conn = create_connection(os.environ['DB_HOST'],
--- app/functions.py
+++ app/functions.py
@@ -104,76 +104,6 @@ def authentification_page(conn):
 
 
 #//////////////////////////////////////////////////////////////////////////////
-#                          Création de la carte interactive
-#//////////////////////////////////////////////////////////////////////////////
-def affichage_ventes_proximite(commune : list, cursor) -> str:
-    """
-    Fonction permettant d'afficher sur une carte les ventes effectuées sur une commune  
-    à l'aide de Folium
-
-    Args :
-    - commune (list) : Liste de type ['Normandie','Calvados','Caen']
-    - cursor: Connexion à la base de données
-    Returns:
-        str (html): <html>.....</html>
-    """
-    # Requête pour sélectionner les ventes sur la communes
-    query=f"""
-    SELECT 
-        v.*,
-        t.NAME_TYPE_BIEN,
-        c.NAME_COMMUNE
-    FROM VENTES v
-    JOIN COMMUNES c ON v.ID_COMMUNE = c.ID_COMMUNE
-    JOIN DEPARTEMENTS d ON c.ID_DEPT = d.ID_DEPT
-    JOIN REGIONS r ON d.ID_REGION = r.ID_REGION
-    JOIN TYPES_BIENS t ON t.ID_TYPE_BIEN=v.ID_TYPE_BIEN
-    WHERE c.NAME_COMMUNE = %s
-        AND d.Name_departement = %s
-        AND r.Name_region = %s;
-    """
-    cursor.execute(query,(commune[2],commune[1],commune[0]))
-    result = cursor.fetchall()
-    # Transformation de la requête en data frame
-    df = pd.DataFrame(result)
-    
-    df.columns= ["id","MONTANT","NUMERO_RUE","RUE","CODE_POSTAL","longitude","latitude",
-                 "DATE_MUTATION","SURFACE_BATI","NOMBRE_PIECES","SURFACE_TERRAIN",
-                 "ID_TYPE_BIEN","xxxxx","CODE_COMMUNE","NAME_TYPE_BIEN","NAME_COMMUNE"]
-    
-    # Création des popup folium
-    df['info'] = df.apply(lambda row: f"""
-    <b>Type de bien :</b><br>{row['NAME_TYPE_BIEN']} <br><br>
-    <b>Date de vente : </b><br>{row['DATE_MUTATION']} <br><br>
-    <b>Adresse : </b><br>{row['NUMERO_RUE']} {row['RUE']} <br>
-    {row['CODE_POSTAL']} {row['NAME_COMMUNE']} <br><br>
-    <b>Surface habitable :</b> {row['SURFACE_BATI']} <br>
-    <b>Surface terrain :</b> {row['SURFACE_TERRAIN']} <br>
-    <b>Nombre de pièces :</b> {row['NOMBRE_PIECES']} <br><br>
-    <b>Montant : </b><br>{row['MONTANT']}
-    """, axis=1)                
-
-    # Centrage de la carte au milieu des coordonnées
-    folium_map = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12)
-
-    # Création d'un cluster de marqueurs
-    marker_cluster = MarkerCluster()
-
-    # Ajout des marqueurs avec les information pour chaque point au cluster
-    for _,row in df.iterrows():
-        location = row['latitude'], row['longitude']
-        marker = folium.Marker(location=location)
-        folium.Popup(f'<div style="font-size: 10px; width: 150px;">{row["info"]}</div>').add_to(marker)
-        marker_cluster.add_child(marker)
-    
-    # Ajout des cluster à la map
-    marker_cluster.add_to(folium_map)
-
-    # Convertir la carte Folium en HTML
-    folium_html = folium_map.get_root().render()
-    return  folium_html
-
-#//////////////////////////////////////////////////////////////////////////////
 #                          Formulaire
 #//////////////////////////////////////////////////////////////////////////////
 def formulaire(cursor):
@@ -268,6 +198,93 @@ def formulaire(cursor):
     else:
         st.write("Veuillez choisir une région")
     return
+
+#//////////////////////////////////////////////////////////////////////////////
+# Affichage des pages en fonctions de l'avancement du remplissage du formulaire
+#//////////////////////////////////////////////////////////////////////////////
+def formulaire_valide():
+    # Appel de l'API
+    prediction = api_predict({'SURFACE_BATI' :st.session_state.surface_bati,
+                            'NB_PIECES' : st.session_state.nb_pieces,
+                            'NAME_TYPE_BIEN':st.session_state.type_de_bien,
+                            'Name_region' : st.session_state.region})
+    # Affichage de la prédiction
+    st.title(f"Le bien est estimé à {int(prediction['reponse'])} €")
+
+    # Affichage des ventes réalisées dans la commune
+    st.components.v1.html(affichage_ventes_proximite([st.session_state.region,
+                                                    st.session_state.departement,
+                                                    st.session_state.commune],cursor), height=500)
+
+#//////////////////////////////////////////////////////////////////////////////
+#                          Création de la carte interactive
+#//////////////////////////////////////////////////////////////////////////////
+def affichage_ventes_proximite(commune : list, cursor) -> str:
+    """
+    Fonction permettant d'afficher sur une carte les ventes effectuées sur une commune  
+    à l'aide de Folium
+
+    Args :
+    - commune (list) : Liste de type ['Normandie','Calvados','Caen']
+    - cursor: Connexion à la base de données
+    Returns:
+        str (html): <html>.....</html>
+    """
+    # Requête pour sélectionner les ventes sur la communes
+    query=f"""
+    SELECT 
+        v.*,
+        t.NAME_TYPE_BIEN,
+        c.NAME_COMMUNE
+    FROM VENTES v
+    JOIN COMMUNES c ON v.ID_COMMUNE = c.ID_COMMUNE
+    JOIN DEPARTEMENTS d ON c.ID_DEPT = d.ID_DEPT
+    JOIN REGIONS r ON d.ID_REGION = r.ID_REGION
+    JOIN TYPES_BIENS t ON t.ID_TYPE_BIEN=v.ID_TYPE_BIEN
+    WHERE c.NAME_COMMUNE = %s
+        AND d.Name_departement = %s
+        AND r.Name_region = %s;
+    """
+    cursor.execute(query,(commune[2],commune[1],commune[0]))
+    result = cursor.fetchall()
+    # Transformation de la requête en data frame
+    df = pd.DataFrame(result)
+    
+    df.columns= ["id","MONTANT","NUMERO_RUE","RUE","CODE_POSTAL","longitude","latitude",
+                 "DATE_MUTATION","SURFACE_BATI","NOMBRE_PIECES","SURFACE_TERRAIN",
+                 "ID_TYPE_BIEN","xxxxx","CODE_COMMUNE","NAME_TYPE_BIEN","NAME_COMMUNE"]
+    
+    # Création des popup folium
+    df['info'] = df.apply(lambda row: f"""
+    <b>Type de bien :</b><br>{row['NAME_TYPE_BIEN']} <br><br>
+    <b>Date de vente : </b><br>{row['DATE_MUTATION']} <br><br>
+    <b>Adresse : </b><br>{row['NUMERO_RUE']} {row['RUE']} <br>
+    {row['CODE_POSTAL']} {row['NAME_COMMUNE']} <br><br>
+    <b>Surface habitable :</b> {row['SURFACE_BATI']} <br>
+    <b>Surface terrain :</b> {row['SURFACE_TERRAIN']} <br>
+    <b>Nombre de pièces :</b> {row['NOMBRE_PIECES']} <br><br>
+    <b>Montant : </b><br>{row['MONTANT']}
+    """, axis=1)                
+
+    # Centrage de la carte au milieu des coordonnées
+    folium_map = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12)
+
+    # Création d'un cluster de marqueurs
+    marker_cluster = MarkerCluster()
+
+    # Ajout des marqueurs avec les information pour chaque point au cluster
+    for _,row in df.iterrows():
+        location = row['latitude'], row['longitude']
+        marker = folium.Marker(location=location)
+        folium.Popup(f'<div style="font-size: 10px; width: 150px;">{row["info"]}</div>').add_to(marker)
+        marker_cluster.add_child(marker)
+    
+    # Ajout des cluster à la map
+    marker_cluster.add_to(folium_map)
+
+    # Convertir la carte Folium en HTML
+    folium_html = folium_map.get_root().render()
+    return  folium_html
 
 
 #//////////////////////////////////////////////////////////////////////////////
