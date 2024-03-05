c35e495e7a3dde310f481f1f37d8d72f3fff7b42
rastakoer <kev.le.grand@orange.fr>
2024-03-05 18:27:57 +0100

    Ajout de print pour debug le non stockage de kpi

--- app/functions.py
+++ app/functions.py
@@ -342,6 +342,13 @@ def log_grafana() -> None:
                     commune=st.session_state.commune,
                     user=st.session_state.username,
                     pred=st.session_state.pred)
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" ,
+          st.session_state.type_de_bien,
+          st.session_state.region,
+          st.session_state.departement,
+          st.session_state.commune,
+          st.session_state.username,
+          st.session_state.pred)
     return
 
 #//////////////////////////////////////////////////////////////////////////////
