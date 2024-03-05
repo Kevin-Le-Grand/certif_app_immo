051743e44164943a1e343f3aa64d35f1bb184712
rastakoer <kev.le.grand@orange.fr>
2024-03-05 14:21:52 +0100

    pages d'accueil

--- app/pages.py
+++ app/pages.py
@@ -6,23 +6,15 @@ import pandas as pd
 engine = sqlengine()
 
 def accueil():
-    st.subheader("Prix moyen en France")
-    for i in ["Appartement","Maison"]:
-        query=f"""SELECT AVG(MONTANT) AS AVGMontant
-        FROM VENTES V
-        INNER JOIN TYPES_BIENS as T ON V.ID_TYPE_BIEN = T.ID_TYPE_BIEN
-        INNER JOIN COMMUNES AS C ON V.ID_COMMUNE = C.ID_COMMUNE
-        INNER JOIN DEPARTEMENTS AS D ON C.ID_DEPT = D.ID_DEPT
-        INNER JOIN REGIONS R ON D.ID_REGION = R.ID_REGION
-        WHERE NAME_TYPE_BIEN='{i}'
-        AND R.Name_region NOT IN("Martinique","Guyane","La Réunion","Mayotte");
-        """
-        datas = pd.read_sql(con=engine.connect(), sql=text(query))
-        col1, col2 = st.columns([3,1])
-        with col1:
-            st.text(i)
-        with col2:
-            st.text(f"{int(datas.iloc[0,0])} €")
+    st.subheader("Cette application web permet d'estimer le montant d'un bien immobilier en France métropolitaine.")
+    # Affichage de la période d'entraînement du modèle
+    query=f"""((SELECT MAX(DATE_MUTATION) FROM VENTES), INTERVAL 15 MONTH)"""
+    df = pd.read_sql(con=engine.connect(), sql=text(query))
+    date_plus_recente = str(df['DATE_MUTATION'].min()).split("")[0]
+    st.write(f"Le modèle a été entraîné sur une période de 12 à partir du {date_plus_recente}")
+    st.write("Il est possible que votre commune ne figure pas dans la liste pour les raisons suivantes :")
+    st.write("  - N'ont été retenues que les communes ayant eu plus de 10 ventes sur la période d'entraînement")
+    st.write("  - N'ont été retenues que les communes ayant moins de 30% de valeurs nettement supérieures à la moyenne nationale")
         
 
 def region():
