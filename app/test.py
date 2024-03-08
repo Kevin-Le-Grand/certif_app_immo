f408d315828082bf977903c1d44894f0f543360f
rastakoer <kev.le.grand@orange.fr>
2024-03-08 09:07:48 +0100

    commentaire dans le fichier test.py

--- app/test.py
+++ app/test.py
@@ -4,7 +4,7 @@ from functions import api_predict,create_connection
 
 class TestAPICalls(unittest.TestCase):
     def test_create_connection(self):
-        # Remplacez ces valeurs par les informations de votre base de données de test
+        # Les informations de connection doivent être renseigné dans le dépôt GitHub
         host = os.environ['DB_HOST']
         user = os.environ['DB_USER']
         password = os.environ['DB_PASSWORD']
@@ -17,25 +17,25 @@ class TestAPICalls(unittest.TestCase):
         # Vérification que le cursor n'est pas None (indicatif d'une connexion réussie)
         self.assertIsNotNone(conn)
 
-        # Fermer la connexion après les tests
+        # Fermeture de  la connexion après les tests
         conn.close()
 
 
     def test_api_predict_success(self):
-        # Appel de la fonction de l'API avec des données factices
+        # Appel de la fonction de l'API avec des données type
         data = {"SURFACE_BATI": 150,
                 "SURFACE_TERRAIN": 750,
                 "prix_moyen_commune_m2": 1356}
         result = api_predict(data)
         json_data = result.json()
 
-        # Vérifier si le résultat est un dictionnaire
+        # Vérification si le résultat est un dictionnaire
         self.assertEqual(dict, type(json_data))
 
-        # Vérifier si la clé 'reponse' est présente dans le JSON
+        # Vérification que la clé 'reponse' est présente dans le JSON
         self.assertIn('reponse', json_data)
 
-        # Vérifier si la valeur de la clé 'reponse' est de type float
+        # Vérification la valeur de la clé 'reponse' est de type float
         self.assertIsInstance(json_data['reponse'], float)
 
 if __name__ == '__main__':
